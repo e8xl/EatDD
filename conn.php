319e5a6f522534937d2eb7453aa8de4d8230670d
e1ghtXL <56244103+e8xl@users.noreply.github.com>
2022-01-23 15:55:49 +0800

    Delete conn.php

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = new mysqli('101.36.109.44:3306','eatxzs','2df4rcShmRSTBdtK','eatxzs');
-mysqli_set_charset($link, 'utf8');
-if ($link->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-}
-$ranking = "kano_rank";//排行榜表名
